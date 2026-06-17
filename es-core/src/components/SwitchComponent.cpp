e66f88b9da65c8fbf24efeb8c8e88b058d15e48d
LeonardWard <pang.e1311@outlook.com>
2026-06-18 01:44:15 +0900

    fix: SwitchComponent 생성자에서 초기 이미지 상태 미반영 버그 수정
    생성자가 항상 off.svg로 이미지를 초기화해서 state=true로 만들어도
    화면에는 꺼짐으로 표시되는 문제. mState에 따라 on/off.svg를 선택하도록 수정.

--- es-core/src/components/SwitchComponent.cpp
+++ es-core/src/components/SwitchComponent.cpp
@@ -4,7 +4,7 @@
 
 SwitchComponent::SwitchComponent(Window* window, bool state) : GuiComponent(window), mImage(window), mState(state)
 {
-	mImage.setImage(":/off.svg");
+	mImage.setImage(mState ? ":/on.svg" : ":/off.svg");
 	mImage.setResize(0, Font::get(FONT_SIZE_MEDIUM)->getHeight());
 	mSize = mImage.getSize();
 }
